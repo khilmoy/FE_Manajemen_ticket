d19bcc966e72f434d41ae478029c47b543323567
rayhnnnaaf <rayhanainuronfalaqi@gmail.com>
2026-07-11 03:17:59 +0700

    resolve merge conflict routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,7 +22,6 @@ Route::get('/beli-ticket', function () {
     return view('ticket.payment');
 })->name('ticket.payment');
 
-<<<<<<< HEAD
 Route::get('/invoice', function () {
     return view('ticket.invoice');
 })->name('ticket.invoice');
@@ -33,25 +31,17 @@ Route::get('/e-ticket', function () {
 })->name('ticket.e-ticket');
 
 Route::get('/admin', function () {
-    return view('admin.index');
-})->name('admin.index');
-=======
-Route::get('admin', function () {
     return view('admin.dashboard');
 })->name('admin.dashboard');
 
-Route::get('admin/kategori', function () {
+Route::get('/admin/kategori', function () {
     return view('admin.kategori');
 })->name('admin.kategori');
 
-Route::get('admin/kategori/create', function () {
+Route::get('/admin/kategori/create', function () {
     return view('admin.kategori.create');
 })->name('admin.kategori.create');
 
-Route::get('admin/kategori/{id}/update', function ($id) {
+Route::get('/admin/kategori/{id}/update', function ($id) {
     return view('admin.kategori.update', ['id' => $id]);
 })->name('admin.kategori.update');
-
->>>>>>> 7baea298d048ee690f7e767976d6b031d7635726
-
-require __DIR__.'/auth.php';
